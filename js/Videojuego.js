a7bb6ade8ec705b38e8b577fc6f9b70fcd2f80f2
Joan de la Huerta <joandelahuertaplanas@gmail.com>
2023-07-02 16:13:29 +0200

    Videojuego

--- js/Videojuego.js
+++ js/Videojuego.js
@@ -1,6 +1,6 @@
-var Videojuego = /** @class */ (function () {
-    function Videojuego(titulo, horasEstimadas, genero, compañia) {
-        if (horasEstimadas === void 0) { horasEstimadas = 10; }
+"use strict";
+class Videojuego {
+    constructor(titulo, horasEstimadas = 10, genero, compañia) {
         this.titulo = titulo;
         this.horasEstimadas = horasEstimadas;
         this.entregado = false;
@@ -8,16 +8,16 @@ var Videojuego = /** @class */ (function () {
         this.compañia = compañia;
     }
     // Implementación Entregable
-    Videojuego.prototype.entregar = function () {
+    entregar() {
         this.entregado = true;
-    };
-    Videojuego.prototype.devolver = function () {
+    }
+    devolver() {
         this.entregado = false;
-    };
-    Videojuego.prototype.isEntregado = function () {
+    }
+    isEntregado() {
         return this.entregado;
-    };
-    Videojuego.prototype.compareTo = function (objeto) {
+    }
+    compareTo(objeto) {
         if (this.horasEstimadas > objeto.getHorasEstimadas()) {
             return 1;
         }
@@ -27,59 +27,42 @@ var Videojuego = /** @class */ (function () {
         else {
             return 0;
         }
-    };
+    }
     //GET Y SET DE TITULO
-    Videojuego.prototype.getTitulo = function () {
+    getTitulo() {
         return this.titulo;
-    };
-    Videojuego.prototype.setTitulo = function (titulo) {
+    }
+    setTitulo(titulo) {
         this.titulo = titulo;
-    };
+    }
     //GET Y SET DE HORASESTIMADAS
-    Videojuego.prototype.getHorasEstimadas = function () {
+    getHorasEstimadas() {
         return this.horasEstimadas;
-    };
-    Videojuego.prototype.setHorasEstimadas = function (horasEstimadas) {
+    }
+    setHorasEstimadas(horasEstimadas) {
         this.horasEstimadas = horasEstimadas;
-    };
+    }
     //GET Y SET DE GENERO
-    Videojuego.prototype.getGenero = function () {
+    getGenero() {
         return this.genero;
-    };
-    Videojuego.prototype.setGenero = function (genero) {
+    }
+    setGenero(genero) {
         this.genero = genero;
-    };
+    }
     //GET Y SET DE COMPAÑIA
-    Videojuego.prototype.getCompañia = function () {
+    getCompañia() {
         return this.compañia;
-    };
-    Videojuego.prototype.setCompañia = function (compañia) {
+    }
+    setCompañia(compañia) {
         this.compañia = compañia;
-    };
+    }
     // SOBRESCRIBE TOSTRING
-    Videojuego.prototype.toString = function () {
-        return "Videojuego:\n      T\u00EDtulo: ".concat(this.titulo, "\n      Horas estimadas: ").concat(this.horasEstimadas, "\n      Entregado: ").concat(this.entregado, "\n      G\u00E9nero: ").concat(this.genero, "\n      Compa\u00F1\u00EDa: ").concat(this.compañia);
-    };
-    return Videojuego;
-}());
-/*
-const videojuego1 = new Videojuego('Videojuego 1', 8, false, 'Compañía 1', 'Compañía 1' );
-const videojuego2 = new Videojuego('Videojuego 2', 2, true, 'Compañía 2', 'Compañía 1' );
-
-videojuego1.entregar();
-console.log('¿Videojuego 1 entregado?', videojuego1.isEntregado());
-
-videojuego2.devolver();
-console.log('¿Videojuego 2 entregado?', videojuego2.isEntregado());
-
-const resultadoComparacion = videojuego1.compareTo(videojuego2);
-if (resultadoComparacion < 0) {
-  console.log('Videojuego 1 tiene menos horas estimadas que Videojuego 2');
-} else if (resultadoComparacion > 0) {
-  console.log('Videojuego 1 tiene más horas estimadas que Videojuego 2');
-} else {
-  console.log('Videojuego 1 y Videojuego 2 tienen la misma cantidad de horas estimadas');
+    toString() {
+        return `Videojuego:
+            Título: ${this.titulo}
+            Horas estimadas: ${this.horasEstimadas}
+            Entregado: ${this.entregado}
+            Género: ${this.genero}
+            Compañía: ${this.compañia}`;
+    }
 }
-
-console.log('Información de Videojuego 1:\n', videojuego1.toString());
-console.log('Información de Videojuego 2:\n', videojuego2.toString());*/ 
