7e304c764513f79aed54e6f042738db5ba24cfd8
Joan de la Huerta <joandelahuertaplanas@gmail.com>
2023-07-02 15:41:41 +0200

    Videojuego

--- js/Videojuego.js
+++ js/Videojuego.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var Videojuego = /** @class */ (function () {
-    function Videojuego(titulo, genero, compañia, horasEstimadas) {
+    function Videojuego(titulo, horasEstimadas, genero, compañia) {
         if (horasEstimadas === void 0) { horasEstimadas = 10; }
         this.titulo = titulo;
         this.horasEstimadas = horasEstimadas;
@@ -64,3 +62,24 @@ var Videojuego = /** @class */ (function () {
     };
     return Videojuego;
 }());
+/*
+const videojuego1 = new Videojuego('Videojuego 1', 8, false, 'Compañía 1', 'Compañía 1' );
+const videojuego2 = new Videojuego('Videojuego 2', 2, true, 'Compañía 2', 'Compañía 1' );
+
+videojuego1.entregar();
+console.log('¿Videojuego 1 entregado?', videojuego1.isEntregado());
+
+videojuego2.devolver();
+console.log('¿Videojuego 2 entregado?', videojuego2.isEntregado());
+
+const resultadoComparacion = videojuego1.compareTo(videojuego2);
+if (resultadoComparacion < 0) {
+  console.log('Videojuego 1 tiene menos horas estimadas que Videojuego 2');
+} else if (resultadoComparacion > 0) {
+  console.log('Videojuego 1 tiene más horas estimadas que Videojuego 2');
+} else {
+  console.log('Videojuego 1 y Videojuego 2 tienen la misma cantidad de horas estimadas');
+}
+
+console.log('Información de Videojuego 1:\n', videojuego1.toString());
+console.log('Información de Videojuego 2:\n', videojuego2.toString());*/ 
