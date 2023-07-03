75cec196f8e21c6d408dd0002459b4f9475dba4c
Joan de la Huerta <joandelahuertaplanas@gmail.com>
2023-07-03 16:46:41 +0200

    Profesor

--- js/Reto04/Profesor.js
+++ js/Reto04/Profesor.js
@@ -1,7 +1,42 @@
 "use strict";
 class Profesor {
-    // ATRIBUTOS
-    // CONSTRUCTORES
-    constructor(parameters) {
+    constructor(nombre, edad, sexo, materia) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.materia = materia;
+    }
+    //GET Y SET DE NOMBRE
+    getNombre() {
+        return this.nombre;
+    }
+    setNombre(nombre) {
+        this.nombre = nombre;
+    }
+    //GET Y SET DE EDAD
+    getEdad() {
+        return this.edad;
+    }
+    setEdad(edad) {
+        this.edad = edad;
+    }
+    //GET Y SET DE SEXO
+    getSexo() {
+        return this.sexo;
+    }
+    setSexo(sexo) {
+        this.sexo = sexo;
+    }
+    //GET Y SET DE MATERIA
+    getMateria() {
+        return this.materia;
+    }
+    setMateria(materia) {
+        this.materia = materia;
+    }
+    // Probabilidad de que el profesor este disponible
+    estaDisponible() {
+        const randomNumber = Math.random();
+        return randomNumber >= 0.2;
     }
 }
--- src/Reto04/Profesor.ts
+++ src/Reto04/Profesor.ts
@@ -1,12 +1,47 @@
 class Profesor implements Usuarios {
+    private nombre: string;
+    private edad: number;
+    private sexo: tSexo;
+    private materia: string;
 
-    // ATRIBUTOS
-
-    // CONSTRUCTORES
-    constructor(parameters) {
-        
+    constructor(nombre: string, edad: number, sexo: tSexo, materia: string) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.materia = materia;
     }
 
-    // MÉTODOS
-    
+    //GET Y SET DE NOMBRE
+    public getNombre(): string {
+        return this.nombre;
+    }
+    public  setNombre(nombre: string): void {
+        this.nombre = nombre;
+    }
+    //GET Y SET DE EDAD
+    public getEdad(): number {
+        return this.edad;
+    }
+    public  setEdad(edad: number): void {
+        this.edad = edad;
+    }
+    //GET Y SET DE SEXO
+    public getSexo(): string {
+        return this.sexo;
+    }
+    public setSexo(sexo: tSexo): void {
+        this.sexo = sexo;
+    }
+    //GET Y SET DE MATERIA
+    public getMateria(): string {
+        return this.materia;
+    }
+    public setMateria(materia: string): void {
+        this.materia = materia;
+    }
+    // Probabilidad de que el profesor este disponible
+    estaDisponible(): boolean {
+        const randomNumber = Math.random();
+        return randomNumber >= 0.2;
+    }
 }
