cb4c584f8dd0138f2304a6dd13a10f16b5380554
Santi Arroyave <135848692+santiarroyave@users.noreply.github.com>
2023-07-11 19:21:40 +0200

    Delete main.js

--- js/Reto03/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let ecuacion = new Raices(1, -3, 2);
-console.log(`Discriminante: ${ecuacion.getDiscriminante()}`);
-console.log(`¿Tiene raíces? ${ecuacion.tieneRaices()}`);
-console.log(`¿Tiene raíz única? ${ecuacion.tieneRaiz()}`);
-ecuacion.calcular();
-ecuacion.obtenerRaices();
-ecuacion.obtenerRaiz();
